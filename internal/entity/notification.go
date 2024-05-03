b4c811e2325d619241da613291c2e3c7996a2255
Vinicius Oliveira <eovinicius10@gmail.com>
2024-05-03 16:53:08 -0300

    ✨ feat: add entity notification

---
+++ internal/entity/notification.go
@@ -0,0 +1,50 @@
+package entity
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Notification struct {
+	ID          uuid.UUID `json:"id"`
+	RecipientID uuid.UUID `json:"recipient_id"`
+	Content     string    `json:"content"`
+	Category    string    `json:"category"`
+	ReadAt      time.Time `json:"read_at"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+func NewNotification(recipientID uuid.UUID, content, category string) (*Notification, error) {
+	notification := &Notification{
+		ID:          uuid.New(),
+		RecipientID: recipientID,
+		Content:     content,
+		Category:    category,
+		ReadAt:      time.Time{},
+		CreatedAt:   time.Now(),
+	}
+	if err := notification.validate(); err != nil {
+		return nil, err
+	}
+
+	return notification, nil
+}
+
+func (n *Notification) MarkAsRead() {
+	n.ReadAt = time.Now()
+}
+
+func (n *Notification) validate() error {
+	if n.RecipientID == uuid.Nil {
+		return fmt.Errorf("recipientID is required")
+	}
+	if n.Content == "" {
+		return fmt.Errorf("content is required")
+	}
+	if n.Category == "" {
+		return fmt.Errorf("category is required")
+	}
+	return nil
+}

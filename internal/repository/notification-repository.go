f71582714a0901028b3de1cf11bb83a7499d33f8
Vinicius Oliveira <eovinicius10@gmail.com>
2024-05-07 16:03:51 -0300

    🦄 refactor: change repository interface to implementation file

--- internal/repository/notification-repository.go
+++ internal/repository/notification-repository.go
@@ -3,19 +3,28 @@ package repository
 import (
 	"database/sql"
 	"github/eovinicius/notification/internal/entity"
+
+	"github.com/google/uuid"
 )
 
-type NotificationRepository struct {
+type NotificationRepository interface {
+	Save(notification *entity.Notification) error
+	FindByID(id uuid.UUID) (*entity.Notification, error)
+	Update(notification *entity.Notification) error
+	FindManyByRecipientID(recipientID string) ([]*entity.Notification, error)
+}
+
+type notificationRepository struct {
 	Db *sql.DB
 }
 
-func NewNotificationRepository(db *sql.DB) *NotificationRepository {
-	return &NotificationRepository{
+func NewNotificationRepository(db *sql.DB) *notificationRepository {
+	return &notificationRepository{
 		Db: db,
 	}
 }
 
-func (r *NotificationRepository) Save(notification *entity.Notification) error {
+func (r *notificationRepository) Save(notification *entity.Notification) error {
 	_, err := r.Db.Exec("INSERT INTO notifications (id, recipient_id, content, category, read_at, created_at) VALUES ($1, $2, $3, $4, $5, $6)", notification.ID, notification.RecipientID, notification.Content, notification.Category, notification.ReadAt, notification.CreatedAt)
 	if err != nil {
 		return err
@@ -24,7 +33,7 @@ func (r *NotificationRepository) Save(notification *entity.Notification) error {
 	return nil
 }
 
-func (r *NotificationRepository) FindByID(id string) (*entity.Notification, error) {
+func (r *notificationRepository) FindByID(id string) (*entity.Notification, error) {
 	var notification entity.Notification
 	err := r.Db.QueryRow("SELECT id, recipient_id, content, category, read_at, created_at FROM notifications WHERE id = $1", id).Scan(&notification.ID, &notification.RecipientID, &notification.Content, &notification.Category, &notification.ReadAt, &notification.CreatedAt)
 	if err != nil {
@@ -34,7 +43,7 @@ func (r *NotificationRepository) FindByID(id string) (*entity.Notification, erro
 	return &notification, nil
 }
 
-func (r *NotificationRepository) Update(notification *entity.Notification) error {
+func (r *notificationRepository) Update(notification *entity.Notification) error {
 	_, err := r.Db.Exec("UPDATE notifications SET recipient_id = $1, content = $2, category = $3, read_at = $4, canceled_at = $5, created_at = $6 WHERE id = $7", notification.RecipientID, notification.Content, notification.Category, notification.ReadAt, notification.CanceledAt, notification.CreatedAt, notification.ID)
 	if err != nil {
 		return err
@@ -43,7 +52,7 @@ func (r *NotificationRepository) Update(notification *entity.Notification) error
 	return nil
 }
 
-func (r *NotificationRepository) FindManyByRecipientID(recipientID string) ([]*entity.Notification, error) {
+func (r *notificationRepository) FindManyByRecipientID(recipientID string) ([]*entity.Notification, error) {
 	rows, err := r.Db.Query("SELECT id, recipient_id, content, category, read_at, created_at FROM notifications WHERE recipient_id = $1", recipientID)
 	if err != nil {
 		return nil, err

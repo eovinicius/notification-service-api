283a50fc1aba68c9c3037c534cb4d2eac96ade7b
Vinicius Oliveira <eovinicius10@gmail.com>
2024-05-09 10:26:56 -0300

    ✨ feat: starting http server with gin gonic

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,64 +1,19 @@
 package main
 
 import (
-	"log"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"github.com/streadway/amqp"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("failed to load .env file: %v", err)
-	}
 
-	conn, err := amqp.Dial(os.Getenv("RABBITMQ_URL"))
-	if err != nil {
-		log.Fatalf("failed to connect to RabbitMQ: %v", err)
-	}
-	defer conn.Close()
+	_ = godotenv.Load()
 
-	ch, err := conn.Channel()
-	if err != nil {
-		log.Fatalf("failed to open a channel: %v", err)
-	}
-	defer ch.Close()
+	app := gin.Default()
 
-	queue, err := ch.QueueDeclare(
-		"notification_queue",
-		false,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		log.Fatalf("failed to declare a queue: %v", err)
-	}
+	PORT := os.Getenv("PORT")
 
-	msgs, err := ch.Consume(
-		queue.Name,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		log.Fatalf("failed to register a consumer: %v", err)
-	}
-
-	forever := make(chan bool)
-
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-		}
-	}()
-
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
+	app.Run(":" + PORT)
 }

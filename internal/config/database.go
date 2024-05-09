78ac6ff833fea8a1d5df9f0012cc53ea2936ba40
Vinicius Oliveira <eovinicius10@gmail.com>
2024-05-09 10:31:34 -0300

    ✨ feat: add sqlite database configuration

--- internal/config/database.go
+++ internal/config/database.go
@@ -1,2 +1,17 @@
 package config
 
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func DbContext() *sql.DB {
+	db, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	return db
+}

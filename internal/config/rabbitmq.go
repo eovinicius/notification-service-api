22bb5d165b3d4db3e8566ef1cb4569a1c8edd2be
Vinicius Oliveira <eovinicius10@gmail.com>
2024-05-10 15:01:58 -0300

    🐞 fix: connection with rabbitmq

--- internal/config/rabbitmq.go
+++ internal/config/rabbitmq.go
@@ -1,23 +1,29 @@
 package config
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/streadway/amqp"
 )
 
-func ConnectRabbitMQ() (*amqp.Connection, error) {
-
+func ConnectMQ() (*amqp.Connection, *amqp.Channel) {
 	_ = godotenv.Load()
-
 	url := os.Getenv("RABBITMQ_URL")
 
 	conn, err := amqp.Dial(url)
 	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
-		return nil, err
+		panic(err)
 	}
-	return conn, nil
+
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	defer ch.Close()
+	
+	return conn, ch
 }
